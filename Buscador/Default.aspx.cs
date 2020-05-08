4bc0603b6c4d6f413a99d01b82da1d0afd267ba7
Antonio Aguilar Sanchez <aguilarsystems@hotmail.com>
2020-05-08 04:07:21 -0500

    Add project files.

---
+++ Buscador/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Buscador
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargaDatosAlumno();
+            }
+        }
+        public void CargaDatosAlumno()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SPSalumnos";
+                cmd.Connection = conn;
+                conn.Open();
+                gvdAlumnos.DataSource = cmd.ExecuteReader();
+                gvdAlumnos.DataBind();
+            }
+        }
+        public void BusquedaAlumno()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connDB"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SPSalumnosPorNombre";
+                cmd.Parameters.Add("@nombre", SqlDbType.VarChar).Value = txtNombreBus.Text.Trim();
+                cmd.Connection = conn;
+                conn.Open();
+                gvdAlumnos.DataSource = cmd.ExecuteReader();
+                gvdAlumnos.DataBind();
+            }
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            BusquedaAlumno();
+        }
+    }
+}
